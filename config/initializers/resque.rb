fb0a1d60098355a61ed114b2847cc692aba2c4fe
lieleme <huayijob@aliyun.com>
2015-08-19 10:09:11 +0800

    all

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,4 +1,4 @@
 require "redis"
-Resque.redis = Redis.new(:url => 'redis://123.57.191.137:6379/15')
+Resque.redis = Redis.new(:url => 'redis://localhost:6379/15')
 Resque.after_fork = Proc.new { ActiveRecord::Base.establish_connection }
 
