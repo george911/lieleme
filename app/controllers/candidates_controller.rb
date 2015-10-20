2dd1f68dcfc14e450606fcc9e57e0f85b2ec98f5
lieleme <huayijob@aliyun.com>
2015-10-20 09:00:27 +0800

    auto

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -4,13 +4,13 @@ class CandidatesController < InheritedResources::Base
   
   def group_email
     @candidates = current_user.candidates.all
-    @candidates = current_user.candidates.where(name:params[:name]) unless params[:name].blank?
-    @candidates = current_user.candidates.where(email:params[:email]) unless params[:email].blank?
-    @candidates = candidates.where(title:params[:title]) unless params[:title].blank?
-    @candidates = candidates.where("year >= ?",params[:year]) unless params[:year].blank?
-    @candidates = candidates.where(city:params[:city]) unless params[:city].blank?
-    @candidates = candidates.where(employer:params[:employer]) unless params[:employer].blank?
-    @candidates = candidates.where("id > 2759")
+    @candidates = @current_user.candidates.where(name:params[:name]) unless params[:name].blank?
+    @candidates = @current_user.candidates.where(email:params[:email]) unless params[:email].blank?
+    @candidates = @candidates.where(title:params[:title]) unless params[:title].blank?
+    @candidates = @candidates.where("year >= ?",params[:year]) unless params[:year].blank?
+    @candidates = @candidates.where(city:params[:city]) unless params[:city].blank?
+    @candidates = @candidates.where(employer:params[:employer]) unless params[:employer].blank?
+    @candidates = @candidates.where("id > 2759")
     @candidates.each do |f|
       sleep 20
       JobNotifier.job_list(f,params[:job_id],params[:content],current_user).deliver_now
