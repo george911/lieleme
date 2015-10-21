af11f798d14384430c9643b928e3b1a851b62223
lieleme <huayijob@aliyun.com>
2015-10-21 20:39:15 +0800

    auto

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -10,7 +10,7 @@ class CandidatesController < InheritedResources::Base
     @candidates = @candidates.where("year >= ?",params[:year]) unless params[:year].blank?
     @candidates = @candidates.where(city:params[:city]) unless params[:city].blank?
     @candidates = @candidates.where(employer:params[:employer]) unless params[:employer].blank?
-    @candidates = @candidates.where("id > 2379")
+    @candidates = @candidates.where("id > 2381")
     @candidates.each do |f|
       sleep 40
       JobNotifier.job_list(f,params[:job_id],params[:content],current_user).deliver_now
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 # 老的seed文件在backup里
-#require File.expand_path('../get_talents', __FILE__)
+require File.expand_path('../get_talents', __FILE__)
 #Candidate.where("title LIKE ? or title Like ?","Objective-C","Swift").each do |f|
 #	f.update(title:"iOS")
 #end
