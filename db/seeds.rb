4e441f67d874aae6519dfb5e027a084d381c3252
lieleme <huayijob@aliyun.com>
2015-10-22 15:43:36 +0800

    auto

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 # 老的seed文件在backup里
-require File.expand_path('../get_talents', __FILE__)
+#require File.expand_path('../get_talents', __FILE__)
 Candidate.where("title LIKE ? and city Like ?","Python","Shanghai").each do |f|
 	f.destory
 end
