ddcccdfeaa56ecbcdd5adc6f13ae785f11db0349
lieleme <huayijob@aliyun.com>
2015-10-29 18:12:12 +0800

    auto

--- app/helpers/candidates_helper.rb
+++ app/helpers/candidates_helper.rb
@@ -1,2 +1,10 @@
 module CandidatesHelper
+  def touch_within_14_days(title,city)
+    if MailHistory.where("created_at > ?", Time.now.days_ago(14)).where("title like ? and city like ?",title,city).present? 
+      return "#e58c7e"
+    else
+      return "#FFF"
+    end
+  end
 end
+
