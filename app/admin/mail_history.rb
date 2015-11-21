a7bf410afb15e163132d50a5cd91d8f10b49f252
lieleme <huayijob@aliyun.com>
2015-11-21 22:51:17 +0800

    auto

--- app/admin/mail_history.rb
+++ app/admin/mail_history.rb
@@ -1,4 +1,4 @@
 ActiveAdmin.register MailHistory do
-  permit_params :job_id,:name, :title, :year, :city,:employer,:job_id,:email,:content,:status
+  permit_params :name, :title, :year, :city,:employer,:job_id,:email,:content,:status
 end
 
